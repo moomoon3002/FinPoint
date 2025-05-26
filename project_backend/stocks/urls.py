3350c6ec7285c3a63f4aca3a5e12f869a2a09fd0
leesangkyung <rmdkle99@naver.com>
2025-05-26 13:29:36 +0900

    예적금, 현물비교

--- project_backend/stocks/urls.py
+++ project_backend/stocks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import StockDataView
 
 urlpatterns = [
-    path('info', views.get_stock_info, name='stock_info'),
+    path('info/', views.get_stock_info, name='stock_info'),
+    path('analyze/', views.analyze_sentiment, name='analyze_sentiment'),
+    path('', StockDataView.as_view(), name='stock-data'),
 ] 
